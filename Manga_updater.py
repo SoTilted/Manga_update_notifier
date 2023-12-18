27321abeee9a51190daada2225150d035ee075a7
SoTilted <103329657+SoTilted@users.noreply.github.com>
2023-12-18 22:22:50 +0200

    Update Database
    Fix sources because sites had changed domains
    
    Remove Schedule because it is now scheduled with cronjob
    
    Update README

--- Manga_updater.py
+++ Manga_updater.py
@@ -4,19 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import pyttsx3
-import schedule
 
 def main():
     engine = pyttsx3.init()
     url='https://www.readmng.com/'#First source.
-    url2='https://www.asurascans.com/'#Second source.
+    url2='https://asuratoon.com/'#Second source.
     url3='https://luminousscans.com/'#Third source.
-    url4='https://reaperscans.com/'#Fourth source.
+    url4='https://reapercomics.com/'#Fourth source.
+    url5='https://mangakatana.com/'#Fifth source.
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
         }#When we request using this, the website thinks its a user and not a bot and we dont get the captcha.
     #<--------------------getting the websites.-------------------->
-    reaperscans = requests.get(url4, headers=headers)
+    #reaperscans = requests.get(url4, headers=headers)
     luminousscans = requests.get(url3, headers=headers)
     asurascans = requests.get(url2, headers=headers)
     readmng=requests.get(url, headers=headers)
@@ -24,22 +24,21 @@ def main():
     doc=BeautifulSoup(readmng.text,"html.parser")
     doc2=BeautifulSoup(asurascans.text,"html.parser")
     doc3=BeautifulSoup(luminousscans.text,"html.parser")
-    doc4=BeautifulSoup(reaperscans.text,"html.parser")
+    #doc4=BeautifulSoup(reaperscans.text,"html.parser")
 
 
     Update_check_dic={}#This dictionary will hold the latest updated manga title and chapter of EVERY source.
 
     #<-------------------------------------updating the checking dictionary.------------------------------------->
-    manga=doc.find(class_='manga_updates')#We find the class that holds the latest manga updates.
-    manga=manga.find_all('dd')#find chapter names and numbers.
-    for i in range(len(manga)):#traverse through the chapters, check if dictionary already has the manga title.
-        if str(manga[i].a.string).strip('\n').split(' - ')[0] in Update_check_dic:
+    manga1=doc.find_all(class_='miniListDesc')#We find the class that holds the latest manga updates.
+    #manga=manga.find_all('miniListDesc')#find chapter names and numbers.
+    manga2=doc.find_all(class_='chapterBox')
+    for i in range(len(manga1)):#traverse through the chapters, check if dictionary already has the manga title.
+        if str(manga1[i].h2.string).strip('\n') in Update_check_dic:
             continue
         else:
-            It_seems_complicated=manga[i].a.string.strip('\n').split(' - ')#It really isn't,we just get the <a> tag of the ith manga
-            #we get the string of it, we strip the new lines of it
-            #and we split it at the'-' because the string format is manga title - chapter number
-            Update_check_dic[It_seems_complicated[0]]=float(It_seems_complicated[1])#add to dictionary.
+            if manga2[i].a is not None and manga1[i].h2 is not None:      
+                Update_check_dic[str(manga1[i].h2.string).strip('\n')]=float(str(manga2[i].a.string).strip('\n'))#add to dictionary.
 
     #<-------------------------------------same thing for second source.------------------------------------->
     #We do the same as above, but because the website is different, we need to find the class that holds
@@ -47,41 +46,33 @@ def main():
     manga=doc2.find_all(class_='luf')
     for i in range(len(manga)):
         if manga[i].h4.string in Update_check_dic:
-            if Update_check_dic[manga[i].h4.string]<float(manga[i].li.a.string.split(' ')[1]):
-                Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
-            else:
-                continue
+            if (manga[i].li.a.string.split(' ')[1]).isdigit():
+                if Update_check_dic[manga[i].h4.string]<float(manga[i].li.a.string.split(' ')[1]):
+                    Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
+                else:
+                    continue
         else:
-            Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
+            if (manga[i].li.a.string.split(' ')[1]).isdigit():
+                Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
     #<-------------------------------------same thing for third source.------------------------------------->
     #This is the exact same code because this website was probably made by the same person as above.
     manga=doc3.find_all(class_='luf')
     for i in range(len(manga)):
         if manga[i].h4.string in Update_check_dic:
+            
             if Update_check_dic[manga[i].h4.string]<float(manga[i].li.a.string.split(' ')[1]):
                 Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
             else:
                 continue
         else:
-            Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
+            if manga[i].li is not None:
+                Update_check_dic[manga[i].h4.string]=float(manga[i].li.a.string.split(' ')[1])
     #<-------------------------------------same thing for fourth source.------------------------------------->
-    #Same idea, different class names and string formats.
-    manga=doc4.find_all(class_='series-box')
-    manga2=doc4.find_all(class_='series-content')
-    for i in range(len(manga)):
-        if manga[i].h5.string[1:] in Update_check_dic:
-            if Update_check_dic[manga[i].h5.string[1:]]<float(manga2[i].span.string.split(' ')[1]):
-                Update_check_dic[manga[i].h5.string[1:]]=float(manga2[i].span.string.split(' ')[1])
-            else:
-                continue
-        else:
-            Update_check_dic[manga[i].h5.string[1:]]=float(manga2[i].span.string.split(' ')[1])
 
     #<-------------------------------------Reading, checking and writting the json file(Database).------------------------------------->
     Updated=[] 
 
-    with open('Manga_Base.json', 'r') as openfile:
-    
+    with open('/home/gerasimos/Downloads/visual-studio-code-bin/Coding/GitHub/Manga_update_notifier/Manga_Base.json', 'r') as openfile:
         # Reading from json file
         Database = json.load(openfile)#this holds our database.
 
@@ -98,29 +89,18 @@ def main():
 
     New_Database = json.dumps(Database, indent = 1)#This is the updated Database.
     # Writing to sample.json
-    with open("Manga_Base.json", "w") as outfile:
+    with open("/home/gerasimos/Downloads/visual-studio-code-bin/Coding/GitHub/Manga_update_notifier/Manga_Base.json", "w") as outfile:
         outfile.write(New_Database)#we overwrite the previous data with the new data.
 
     #print(len(json_object))
     if len(Updated)==0:#If the list is empty, it means no new manga updated since last check.
-        #print('No new manga updates since last scheduled check.\n') #I dont use this print since i run this script on start-up of my laptop.
+        print('No new manga updates since last scheduled check.\n') #I dont use this print since i run this script on start-up of my laptop.
         engine.say('No new manga updates since last scheduled check.')#text to speech
         engine.runAndWait()
     else:
         for manga in Updated:#Prints the manga title, chapter and how many unread chapters i have.
-            #print(f'{manga[0]} chapter {manga[1]} has been updated.\nYou have not read the last {int(manga[2])} chapters.')#Same reason as before.
+            print(f'{manga[0]} chapter {manga[1]} has been updated.\nYou have not read the last {int(manga[2])} chapters.')#Same reason as before.
             engine.say(f'{manga[0]} chapter {manga[1]} has been updated.\nYou have not read the last {int(manga[2])} chapters.')#text to speech
             engine.runAndWait()
-    return schedule.CancelJob
-counter=0
 
-while True:
-    if counter==0:#We want to execute main once on start-up of the code
-        schedule.every(5).seconds.do(main)
-        counter+=1
-        time.sleep(6)
-        
-    else:#we schedule to run the code every 6 hours
-        schedule.every(1).hour.do(main)
-        time.sleep(3590)
-    schedule.run_pending()
+main()
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Manga_update_notifier
-This code gets the latest manga updates from 4 different sources(basic web scrapping)
+This code gets the latest manga updates from many different sources(basic web scrapping)
 and then it searches my Database(basic use of json files) and checks if there is a new update.
 Then, with the use of a text to speech library, it tells me if there was an update or not.
-This code is scheduled to run once upon execution and then every 6 hours, this script can be run by hand or on start-up.
+This code is scheduled to run every hour.
 # TO DO: 
- Fix/update the Database(basically the manga i read).By fix i mean type the last chapter i've read on some manga that i just initialized them to zero.
- Maybe add more sources if needed.
+ Fix/update the Database(Add mangas to the jason file).
+ Maybe add more sources and fix the current ones.
  Add as dictionary value which website updated the latest chapter of that manga title.
